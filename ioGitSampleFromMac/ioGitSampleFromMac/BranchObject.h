6cce012c09d4559b6374faf8e20d5b3a576e09d4
Darsan Geevarghese <darsangeevarghese@hq.microobjects.net>
2015-10-15 13:09:20 +0530

    branch object added

---
+++ ioGitSampleFromMac/ioGitSampleFromMac/BranchObject.h
@@ -0,0 +1,13 @@
+//
+//  BranchObject.h
+//  ioGitSampleFromMac
+//
+//  Created by Darsan Geevarghese on 15/10/15.
+//  Copyright © 2015 Darsan Geevarghese. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BranchObject : NSObject
+
+@end
---
+++ ioGitSampleFromMac/ioGitSampleFromMac/BranchObject.m
@@ -0,0 +1,13 @@
+//
+//  BranchObject.m
+//  ioGitSampleFromMac
+//
+//  Created by Darsan Geevarghese on 15/10/15.
+//  Copyright © 2015 Darsan Geevarghese. All rights reserved.
+//
+
+#import "BranchObject.h"
+
+@implementation BranchObject
+
+@end
